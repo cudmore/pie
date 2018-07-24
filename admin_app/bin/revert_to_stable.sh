0e120f2ca224353b66b9047d48760ea8f67405a0
cudmore <robert.cudmore@gmail.com>
2018-07-24 07:44:32 -0400

    revert to stable

---
+++ admin_app/bin/revert_to_stable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Robert Cudmore
+# 20180712
+
+#revert_to_stable.sh
+
+ip=`hostname -I | xargs`
+
+timestamp="[$HOSTNAME] "$(date '+%Y-%m-%d %H:%M:%S')" : "
+
+echo "$timestamp === Admin server is at http://$ip:5011"
+
+echo "$timestamp === Revert is not implemented"
