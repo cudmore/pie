a5ff46adb96b73b82dd603b2a0ffcb9d1cabc8fb
cudmore <robert.cudmore@gmail.com>
2018-12-23 20:28:46 -0500

    commander osx

---
+++ commander_app/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+#exec (open('pymapmanager/version.py').read())
+
+setup(
+    name='pie',
+    version="1.0.0",
+    description='Raspberry Pi Controlled Experiment (PiE)',
+    url='http://github.com/cudmore/pie',
+    author='Robert H Cudmore',
+    author_email='robert.cudmore@gmail.com',
+    license='GNU GPLv3',
+    packages = find_packages(),
+    install_requires=[
+        "flask",
+        "flask-cors",
+        "paramiko"
+    ]
+)
